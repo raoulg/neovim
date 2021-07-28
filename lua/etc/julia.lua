e8933ff7e4b5bcd63210a5d18f696c5030abaa2f
ExpandingMan <savastio@protonmail.com>
2021-07-28 10:42:22 -0400

    fixed julia bracket alignment

--- lua/etc/julia.lua
+++ lua/etc/julia.lua
@@ -1,3 +1,6 @@
+vim.g.julia_indent_align_brackets = 1
+vim.g.julia_indent_align_funcargs = 1
+
 WhichKey.register({
     o = {":call julia#toggle_function_blockassign()<CR>", "toggle Julia function block", noremap=true},
 }, {prefix="<leader>"})
