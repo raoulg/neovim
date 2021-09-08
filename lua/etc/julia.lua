83188b5948823d10383bbeee614a13d42c5e9bf0
ExpandingMan <savastio@protonmail.com>
2021-09-08 13:30:29 -0400

    maybe tab issue is conflict with latex-to-unicode... disabled tab for that without losing functionality

--- lua/etc/julia.lua
+++ lua/etc/julia.lua
@@ -1,5 +1,7 @@
 vim.g.julia_indent_align_brackets = 1
 vim.g.julia_indent_align_funcargs = 1
+-- disable tab in favor of Cmp completion, but still works in command mode
+vim.g.latex_to_unicode_tab = "command"
 
 WhichKey.register({
     o = {":call julia#toggle_function_blockassign()<CR>", "toggle Julia function block", noremap=true},
