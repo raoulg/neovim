ce7dad1411c6a51d7ca2106f763dd7f5bd00977c
Expanding Man <savastio@protonmail.com>
2021-11-04 14:24:44 -0400

    added minimal config for debugging

---
+++ minimal.lua
@@ -0,0 +1,10 @@
+vim.o.encoding = "utf-8"
+
+-- no bootstrapping for packer, assumes already installed
+Packer = require("packer")
+Packer.startup(function()
+    -- TEST MODULES HERE
+end)
+
+
+-- TESTING CONFIG HERE
