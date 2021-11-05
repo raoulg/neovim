ad3dea4099b28dde9a93a4753ffbf112336a02ee
Expanding Man <savastio@protonmail.com>
2021-11-05 09:58:49 -0400

    added note about packer in minimal config

--- minimal.lua
+++ minimal.lua
@@ -1,5 +1,7 @@
 vim.o.encoding = "utf-8"
 
+-- TODO: supposedly packer silently loads packages so you may have to figure something else out
+
 -- no bootstrapping for packer, assumes already installed
 Packer = require("packer")
 Packer.startup(function()
