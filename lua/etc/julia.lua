a7dc853ea38ddabffda14ed4ef7af3f2b9ef8724
ExpandingMan <savastio@protonmail.com>
2022-04-26 10:07:50 -0400

    temporarily disable commandline latex to unicode because it was messing with wilder

--- lua/etc/julia.lua
+++ lua/etc/julia.lua
@@ -1,7 +1,10 @@
 vim.g.julia_indent_align_brackets = 1
 vim.g.julia_indent_align_funcargs = 1
 -- disable tab in favor of Cmp completion, but still works in command mode
-vim.g.latex_to_unicode_tab = "command"
+-- TODO: this is temporarily disabled because it partially breaks wilder.nvim
+-- see https://github.com/JuliaEditorSupport/julia-vim/issues/291
+--vim.g.latex_to_unicode_tab = "command"
+vim.g.latex_to_unicode_tab = "off"
 
 WhichKey.register({
     o = {":call julia#toggle_function_blockassign()<CR>", "toggle Julia function block", noremap=true},
