e737ddaf6d65db57578a1a973f2a515d47e75338
ExpandingMan <savastio@protonmail.com>
2022-08-23 18:54:38 -0400

    restored disabled latex-to-unicode option thats no longer relevant

--- lua/etc/julia.lua
+++ lua/etc/julia.lua
@@ -1,9 +1,6 @@
 vim.g.julia_indent_align_brackets = 1
 vim.g.julia_indent_align_funcargs = 1
--- disable tab in favor of Cmp completion, but still works in command mode
--- TODO: this is temporarily disabled because it partially breaks wilder.nvim
--- see https://github.com/JuliaEditorSupport/julia-vim/issues/291
---vim.g.latex_to_unicode_tab = "command"
+vim.g.latex_to_unicode_tab = "command"
 vim.g.latex_to_unicode_tab = "off"
 
 WhichKey.register({
