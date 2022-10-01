d33373d30b5df0f225a55bf6b12c54864391d645
ExpandingMan <savastio@protonmail.com>
2022-10-01 13:18:14 -0400

    fix registers.nvim deprecations

--- lua/etc/registers.lua
+++ lua/etc/registers.lua
@@ -1,7 +1,10 @@
-vim.g.registers_register_key_sleep = 0
-vim.g.registers_show_empty_registers = 1
-vim.g.registers_trim_whitespace = 1
-vim.g.registers_hide_only_whitespace = 0
-vim.g.registers_window_border = "none"
-vim.g.reigsters_window_min_height = 3
-vim.g.registers_window_max_width = 100
+local Registers = require("registers")
+
+Registers.setup {
+    trim_whitespace=true,
+    window = {
+        max_width=100,
+        border="none",
+        transparency=20,
+    },
+}
--- lua/etc/whichkey.lua
+++ lua/etc/whichkey.lua
@@ -3,7 +3,7 @@ WhichKey = require("which-key")
 WhichKey.setup({
     plugins = {
         marks = true, -- shows a list of your marks on ' and `
-        registers = true, -- shows your registers on " in NORMAL or <C-r> in INSERT mode
+        registers = false, -- this is handled by registers.nvim instead
         spelling = {
             enabled = false, -- enabling this will show WhichKey when pressing z= to select spelling suggestions
             suggestions = 20, -- how many suggestions should be shown in the list?
