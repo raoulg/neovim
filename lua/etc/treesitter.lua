6e3e55347aab34c2d05d6eed9e46c35ddb722ae0
ExpandingMan <savastio@protonmail.com>
2022-10-10 20:36:48 -0400

    use PR for fixing julia treesitter until it is merged

--- lua/etc/treesitter.lua
+++ lua/etc/treesitter.lua
@@ -1,12 +1,26 @@
 local TSConfig = require("nvim-treesitter.configs")
+local TSParsers = require("nvim-treesitter.parsers")
+
+-- TODO: until this PR is merged
+-- https://github.com/tree-sitter/tree-sitter-julia/pull/54
+-- this is for a custom parser
+local parser_config = TSParsers.get_parser_configs()
+parser_config.julia = {
+    install_info = {
+        url = "https://github.com/savq/tree-sitter-julia/",
+        branch = "dev", 
+    },
+    filetype = "jl",
+}
+
 
 TSConfig.setup {
-    ensure_installed = {"c", "lua", "rust", "zig"},
+    ensure_installed = {"c", "lua", "rust", "zig", "julia"},
     auto_install = true,
     highlight = {
         -- vimtex doesn't like treesitter doing the highlighting
         -- the markdown and julia TS grammars both suck
-        disable = {"latex", "markdown", "julia"},
+        disable = {"latex", "markdown"}--, "julia"},
         enable = true,
     },
     additional_vim_regex_highlighting = true,
