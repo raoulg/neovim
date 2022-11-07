ebd77376ec1e868a795ad6d40dde95f770fa13f1
ExpandingMan <savastio@protonmail.com>
2022-11-07 10:56:44 -0500

    turn back on latex-to-unicode from julia-vim until I can fully replace the functionality

--- lua/etc/julia.lua
+++ lua/etc/julia.lua
@@ -1,7 +1,7 @@
 vim.g.julia_indent_align_brackets = 1
 vim.g.julia_indent_align_funcargs = 1
-vim.g.latex_to_unicode_tab = "command"
-vim.g.latex_to_unicode_tab = "off"
+vim.g.latex_to_unicode_tab = "cmd"
+vim.g.latex_to_unicode_auto = 1
 
 WhichKey.register({
     o = {":call julia#toggle_function_blockassign()<CR>", "toggle Julia function block", noremap=true},
