aa1ff87b873cb97a93549d2ce6195b10e9c6c407
Raoul Grouls <raoul.grouls@businessdecision.nl>
2022-12-27 16:38:56 +0100

    toggleterm

--- init.lua
+++ init.lua
@@ -49,6 +49,7 @@ require('packer').startup(function(use)
   use 'rbong/vim-flog'
   use 'folke/tokyonight.nvim'
   use 'jpalardy/vim-slime'
+  use 'akinsho/toggleterm.nvim'
 
   -- Git related plugins
   use 'tpope/vim-fugitive'
@@ -181,6 +182,37 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end,
   group = highlight_group,
   pattern = '*',
+})
+
+local function termsize(term)
+    if term.direction == "horizontal" then
+        return 20
+    elseif term.direction == "vertical" then
+        return vim.o.columns * 0.3
+    end
+end
+
+require("toggleterm").setup({
+    size = termsize,
+    open_mapping = "<C-\\>",
+    hide_numbers = false,
+    shade_terminals = true,
+    start_in_insert = true,
+    insert_mappings = true,
+    persist_size = true,
+    direction = "horizontal",
+    close_on_exit = true,
+    shell = vim.o.shell,  -- set the default shell
+    float_opts = {
+        border = "curved",  -- can be "double", "shadow", "curved" or "single"
+        --width = 80,
+        --height = 60,
+        winblend = 3,
+        --highlights = {
+        --    border = "ToggleTermBorder",
+        --    background = "ToggleTermBackground",
+        --}
+    },
 })
 
 -- Set lualine as statusline
