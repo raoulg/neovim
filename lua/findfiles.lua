6e2dc920b93bc3cb63aad1281beaadc83e1b6221
Raoul Grouls <raoul.grouls@businessdecision.nl>
2022-12-27 20:32:40 +0100

    split files

--- lua/findfiles.lua
+++ lua/findfiles.lua
@@ -20,25 +20,24 @@ WhichKey = require("which-key")
 WhichKey.register({
   f = {
     name = "Find everything",
-    f = {"<cmd>Telescope oldfiles<CR>", "Find recent files"},
+    f = {"<cmd>Telescope find_files<CR>", "Find files"},
+    r = {"<cmd>Telescope oldfiles<CR>", "Find recent files"},
     b = {"<cmd>Telescope buffers<CR>", "Find recent buffers"},
+    h = {"<cmd>Telescope help_tags<CR>", "Search help"},
+    w = {"<cmd>Telescope grep_string<CR>", "Search current word"},
+    g = {"<cmd>Telescope live_grep<CR>", "Grep"},
+    d = {"<cmd>Telescope diagnostics<CR>", "Search diagnostics"},
+    k = {"<cmd>Telescope keymaps<CR>", "Search keymaps"},
   },
 }, {prefix="<leader>"})
 
 -- See `:help telescope.builtin`
-vim.keymap.set('n', '<leader>/', function()
+vim.keymap.set('n', '<leader>f/', function()
   -- You can pass additional configuration to telescope to change theme, layout, etc.
   require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
     winblend = 10,
     previewer = false,
   })
 end, { desc = '[/] Fuzzily search in current buffer]' })
-
-vim.keymap.set('n', '<leader>sf', require('telescope.builtin').find_files, { desc = '[S]earch [F]iles' })
-vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags, { desc = '[S]earch [H]elp' })
-vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { desc = '[S]earch current [W]ord' })
-vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
-vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
-vim.keymap.set('n', '<leader>sk', require('telescope.builtin').keymaps, { desc = '[S]earch [K]eymaps' })
 
 
