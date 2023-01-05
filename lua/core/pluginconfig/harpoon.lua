82281580b5cbcf6bbee978ff29f59cd6092e84e4
Raoul Grouls <raoul.grouls@businessdecision.nl>
2023-01-05 13:26:29 +0100

    harpoon keybinding

--- lua/core/pluginconfig/harpoon.lua
+++ lua/core/pluginconfig/harpoon.lua
@@ -3,6 +3,7 @@ WhichKey.register({
         name = "harpoon",
         a = {"<cmd>lua require'harpoon.mark'.add_file()<CR>", "Add file"},
         m = {"<cmd>lua require'harpoon.ui'.toggle_quick_menu()<CR>", "Toggle quick menu"},
+        h = {"<cmd>Telescope harpoon marks<CR>", "Fuzzy find harpoon marks"},
         ["1"] = {"<cmd>lua require'harpoon.ui'.nav_file(1)<CR>", "Goto file 1"},
         ["2"] = {"<cmd>lua require'harpoon.ui'.nav_file(2)<CR>", "Goto file 2"},
         ["3"] = {"<cmd>lua require'harpoon.ui'.nav_file(3)<CR>", "Goto file 3"},
