8be66bce4d7a733ccafe6d585a8508f1c09e0a8c
Raoul Grouls <raoul.grouls@businessdecision.nl>
2023-01-05 13:27:25 +0100

    hidden files in neotree

--- lua/core/pluginconfig/neotree.lua
+++ lua/core/pluginconfig/neotree.lua
@@ -2,13 +2,33 @@ Neotree = require("neo-tree")
 
 Neotree.setup({
   close_if_last_window = true,
+
   filesystem = {
     hijack_netrw_behavior = "open_default",
     use_libuv_file_watcher = true,
+    filtered_items = {
+      visible = false, -- when true, they will just be displayed differently than normal items
+      hide_dotfiles = true,
+      hide_gitignored = true,
+      hide_by_pattern = { -- uses glob style patterns
+        --"*.meta",
+        --"*/src/*/tsconfig.json",
+      },
+      never_show = { -- remains hidden even if visible is toggled to true, this overrides always_show
+        ".DS_Store",
+        ".git",
+        ".vscode",
+      },
+      never_show_by_pattern = { -- uses glob style patterns
+        --".null-ls_*",
+      },
+    },
   },
+
   buffers = {
-    follow_current_file = false,
+    follow_current_file = true,
   },
+
   event_handlers = {
     {
       event = "neo_tree_buffer_enter",
