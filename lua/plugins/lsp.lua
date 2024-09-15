5688d6679951603bef9b1894d6cd9a5d96796d6b
Raoul Grouls <Raoul.Grouls@han.nl>
2024-09-15 22:46:46 +0200

    fix lua diagnostics

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -48,16 +48,44 @@ return {
 					end,
 				},
 			})
+			local lspconfig = require("lspconfig")
 
 			-- find venv
-			require("lspconfig").pyright.setup({
+			lspconfig.pyright.setup({
 				on_init = function(client)
 					client.config.settings.python.venvPath = vim.fn.getcwd()
 					client.config.settings.python.pythonPath = vim.fn.getcwd() .. "/.venv/bin/python"
 				end,
 			})
 
-			require("lspconfig").ruff.setup({})
+			lspconfig.ruff.setup({})
+
+			lspconfig.lua_ls.setup({
+				settings = {
+					Lua = {
+						runtime = {
+							-- Tell the language server which version of Lua you're using
+							-- (most likely LuaJIT in the case of Neovim)
+							version = "LuaJIT",
+						},
+						diagnostics = {
+							-- Get the language server to recognize the `vim` global
+							globals = {
+								"vim",
+								"require",
+							},
+						},
+						workspace = {
+							-- Make the server aware of Neovim runtime files
+							library = vim.api.nvim_get_runtime_file("", true),
+						},
+						-- Do not send telemetry data containing a randomized but unique identifier
+						telemetry = {
+							enable = false,
+						},
+					},
+				},
+			})
 		end,
 	},
 
