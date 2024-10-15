fee64b2cc1bd8a3c60d60a3b9c09fa2f581660f1
Raoul Grouls <Raoul.Grouls@han.nl>
2024-10-15 20:30:28 +0200

    remove flash, add leap

--- lua/plugins/finding.lua
+++ lua/plugins/finding.lua
@@ -1,6 +1,10 @@
--- require'telescope'.load_extension('project')
-
 return {
+	{
+		"ggandor/leap.nvim",
+		config = function()
+			require("leap").create_default_mappings()
+		end,
+	},
 	{
 		"danielfalk/smart-open.nvim",
 		branch = "0.2.x",
@@ -15,27 +19,7 @@ return {
 			"nvim-telescope/telescope-fzy-native.nvim",
 		},
 	},
-	-- {
-	-- 	"nvim-telescope/telescope-frecency.nvim",
-	-- 	config = function()
-	-- 		require("telescope").load_extension("frecency")
-	-- 	end,
-	-- },
 	{ "folke/which-key.nvim", lazy = true },
-	{
-		"folke/flash.nvim",
-		event = "VeryLazy",
-		---@type Flash.Config
-		opts = {},
-		  -- stylua: ignore
-		  keys = {
-			{ "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
-			{ "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
-			{ "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
-			{ "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
-			{ "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
-		  },
-	},
 	{ "nvim-telescope/telescope.nvim", dependencies = { "nvim-lua/plenary.nvim" } },
 	{ "tsakirist/telescope-lazy.nvim" },
 	{
