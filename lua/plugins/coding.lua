c42f5b95940224d25462a7152428664664ea2f2b
raoul grouls <Raoul.Grouls@han.nl>
2024-12-14 12:48:37 +0100

    add git blame, remove lazy loading for copilot

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -1,5 +1,22 @@
 return {
 	{
+		"f-person/git-blame.nvim",
+		-- load the plugin at startup
+		event = "VeryLazy",
+		-- Because of the keys part, you will be lazy loading this plugin.
+		-- The plugin wil only load once one of the keys is used.
+		-- If you want to load the plugin at startup, add something like event = "VeryLazy",
+		-- or lazy = false. One of both options will work.
+		opts = {
+			-- your configuration comes here
+			-- for example
+			enabled = true, -- if you want to enable the plugin
+			message_template = " <summary> • <date> • <author> • <<sha>>", -- template for the blame message, check the Message template section for more options
+			date_format = "%m-%d-%Y %H:%M:%S", -- template for the date, check Date format section for more options
+			virtual_text_column = 1, -- virtual text start column, check Start virtual text at column section for more options
+		},
+	},
+	{
 		"AckslD/nvim-neoclip.lua",
 		dependencies = {
 			-- you'll need at least one of these
@@ -249,9 +266,9 @@ return {
 	-- },
 	{
 		"zbirenbaum/copilot.lua",
-		lazy = true,
+		lazy = false,
 		cmd = "Copilot",
-		event = "InsertEnter",
+		-- event = "InsertEnter",
 		config = function()
 			require("copilot").setup({
 				suggestion = { enabled = true },
@@ -262,8 +279,8 @@ return {
 	},
 	{
 		"zbirenbaum/copilot-cmp",
-		lazy = true,
-		event = "InsertEnter",
+		lazy = false,
+		-- event = "InsertEnter",
 		opts = {},
 	},
 	{
