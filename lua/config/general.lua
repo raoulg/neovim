e77cb9366a180e4a135bf3c6adb08cdb4002d627
raoul grouls <Raoul.Grouls@han.nl>
2025-08-27 15:40:27 +0200

    localleader

--- lua/config/general.lua
+++ lua/config/general.lua
@@ -23,7 +23,7 @@ COLORS = {
 -- loading lazy.nvim so that mappings are correct.
 -- This is also a good place to setup other settings (vim.opt)
 vim.g.mapleader = " "
-vim.g.maplocalleader = "\\"
+-- vim.g.maplocalleader = "\\"
 
 vim.opt.colorcolumn = "88"
 
